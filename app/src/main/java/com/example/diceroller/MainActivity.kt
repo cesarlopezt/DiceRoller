d69451968dea8c0201489c7ace2f99eb6feb38d1
cesarlopezt <cesarlopez2808@gmail.com>
2022-10-09 00:43:06 -0400

    Add logs

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
+
+private const val TAG = "MainActivity"
 
 /**
  * This activity allows the user to roll a dice and view the result
@@ -29,15 +32,19 @@ class MainActivity : AppCompatActivity() {
         val dice = Dice(6)
 
         // Update the screen with the two dice rolls
-        val diceImg1: ImageView = findViewById(R.id.imageView)
-        val drawableDice1 = getDiceDrawable(dice.roll())
+        val diceImg1 = findViewById<ImageView>(R.id.imageView)
+        val diceRoll1 = dice.roll()
+        val drawableDice1 = getDiceDrawable(diceRoll1)
         diceImg1.setImageResource(drawableDice1)
         diceImg1.contentDescription = drawableDice1.toString()
+        Log.i(TAG, "Dice 1 rolls in $diceRoll1")
 
-        val diceImg2: ImageView = findViewById(R.id.imageView2)
-        val drawableDice2 = getDiceDrawable(dice.roll())
+        val diceImg2 = findViewById<ImageView>(R.id.imageView2)
+        val diceRoll2 = dice.roll()
+        val drawableDice2 = getDiceDrawable(diceRoll2)
         diceImg2.setImageResource(drawableDice2)
         diceImg2.contentDescription = drawableDice2.toString()
+        Log.i(TAG, "Dice 2 rolls in $diceRoll2")
     }
 
     /**
